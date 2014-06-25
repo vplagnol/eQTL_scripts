19fc7e742bbb562f0bcbc36892c0347e725ab6c3
Vincent Plagnol <vincent.plagnol@gmail.com>
2014-06-25 12:29:59 +0100

    Modified Kitty's submission script

--- coloc/submit_eqtl_eqtl.R
+++ coloc/submit_eqtl_eqtl.R
@@ -9,6 +9,15 @@ coloc.eqtl.eqtl <- function(dataset1,
 
   path_to_Rscript = '/share/apps/R-3.0.1/bin/Rscript'
 
+  eqtl.dataset1 <- names(dataset1)
+  eqtl.dataset2 <- names(dataset2)
+  cond.1 <- as.character(dataset1[[1]])
+  cond.2 <- as.character(dataset2[[1]])
+
+  message('Dataset 1 ', eqtl.dataset1, ' and condition 1 ', cond.1)
+  message('Dataset 2 ', eqtl.dataset2, ' and condition 2 ', cond.2)
+  
+  
   # Create cluster folders: 
   cluster.out = paste(cluster.folder, '/out', sep='')
   if (!file.exists (cluster.out)) dir.create(cluster.out)
@@ -17,8 +26,12 @@ coloc.eqtl.eqtl <- function(dataset1,
   cluster.submission = paste(cluster.folder, '/submission', sep='')
   if (!file.exists (cluster.submission)) dir.create(cluster.submission)
   
-  main_script = paste(basedir, "/coloc/main_eqtl_eqtl.R", sep ='') 
-  outfld = paste(outdir, "/results/", eqtl.dataset1, "_", eqtl.dataset2, "/", cond.1, "_", cond.2, sep = '') 
+  main_script = paste(basedir, "/coloc/main_eqtl_eqtl.R", sep ='')
+  outfld.base <-  paste(outdir, "/", eqtl.dataset1, "_", eqtl.dataset2, sep = '')
+  outfld = paste(outdir, "/", eqtl.dataset1, "_", eqtl.dataset2, "/", cond.1, "_", cond.2, sep = '') 
+
+  if (!file.exists(outfld.base)) dir.create( outfld.base )
+  if (!file.exists(outfld)) dir.create( outfld )
   
   script_to_submit = paste(cluster.submission, '/Submit_main_script_', eqtl.dataset1, '_', eqtl.dataset2, '_', cond.1, '_', cond.2, '.sh', sep='') 
   
@@ -29,8 +42,12 @@ coloc.eqtl.eqtl <- function(dataset1,
           cond.1, cond.2, outfld, basedir, sep=' '), append=TRUE)
 
   message('R script file written in ', main_script)
+  message('Submission script: ', script_to_submit)
 
-  #system(paste('qsub -cwd -l  h_vmem=8G -l tmem=8G -l h_rt=20:0:0 -o ', cluster.out, ' -e ',  cluster.error, ' ',  script_to_submit, sep=''))  
+  if (!file.exists(main_script)) {stop('The main R submission script does not exist')}
+  if (!file.exists(script_to_submit)) {stop('The main bash submission script does not exist')}
+  
+  system(paste('qsub -cwd -l  h_vmem=8G -l tmem=8G -l h_rt=20:0:0 -o ', cluster.out, ' -e ',  cluster.error, ' ',  script_to_submit, sep=''))  
 }
 
 
@@ -53,7 +70,7 @@ eqtl.dataset1 <- 'LCL_dexamethasone_DiRienzo'
 eqtl.dataset2 <- 'WB_dexamethasone_DiRienzo'  
 cond.1="logFC"
 cond.2="logFC"
-basedir = "/cluster/project8/vyp/eQTL_integration"
+basedir = "/cluster/project8/vyp/eQTL_integration/scripts"
 outdir = "/cluster/project8/vyp/eQTL_integration/coloc"
 cluster.folder = '/cluster/project8/vyp/eQTL_integration/cluster'
 
