a38f6429fafb089a40e6390b153c244227959409
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2014-07-23 16:17:26 +0100

    finalisation

---
+++ gwas_scripts/defunct/runRonCluster.sh
@@ -0,0 +1,7 @@
+f=$1
+y=${f%R}
+script=$y"sh"
+cp /cluster/project8/vyp/eQTL_integration/scripts/gwas/summaryStats/runRonCluster_input.sh $script
+echo R CMD BATCH --no-save $f >> $script
+echo "Running" $f "on cluster as" $script
+qsub $script
